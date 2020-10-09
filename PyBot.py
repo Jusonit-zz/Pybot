a3f07dc2d31bac26fffcae35a9b0856d9f0a34cd
Junior Figuereo <juniorsj@live.com>
2020-10-09 18:24:02 -0400

    login protocols done

--- PyBot.py
+++ PyBot.py
@@ -1,41 +1,17 @@
-from time import sleep
+from selenium.webdriver.common.keys import Keys
+from Login import LoginProtocols
 from selenium import webdriver
+from random import randint
+from time import sleep
 import sys
 
-#credentials from command line arguments, or manually edit here if preferred
-username = sys.argv[1]
-password = sys.argv[2]
+if __name__ == "__main__":
+    
+    username = "username"
+    password = "password"
+    browser = webdriver.Firefox()
+    browser.implicitly_wait(5) #if selenium can't find an element, it waits 5 seconds and tries again
+    Log = LoginProtocols(browser)
+    Log.login(username, password, browser)
 
-#start browser
-browser = webdriver.Firefox()
-
-#sets five seconds of waiting time
-#if selenium can't find an element, it waits 5 seconds and tries again
-browser.implicitly_wait(5)
-
-#types url on the address bar and hits enter
-browser.get('https://www.instagram.com/')
-
-#to let page load
-sleep(2)
-
-#finds username and password inputs by css
-username_input = browser.find_element_by_css_selector("input[name='username']")
-password_input = browser.find_element_by_css_selector("input[name='password']")
-
-#types your username and password.
-username_input.send_keys(username)
-password_input.send_keys(password)
-#or if manually adding it here
-#username_input.send_keys("your_username")
-#password_input.send_keys("your_password")
-
-sleep(2)
-
-#finds the login button and clicks
-login_button = browser.find_element_by_xpath("//button[@type='submit']")
-login_button.click()
-
-sleep(5)
-
-browser.close()
+    browser.close()
